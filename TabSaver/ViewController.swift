55729287a814bd24a2efaf91f72316ab386de713
Hyunchel Kim <hyunchel.inbox@gmail.com>
2017-09-16 19:57:03 -0500

    Initial Commit

---
+++ TabSaver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TabSaver
+//
+//  Created by Hyunchel Kim on 9/16/17.
+//  Copyright © 2017 Hyunchel Kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
